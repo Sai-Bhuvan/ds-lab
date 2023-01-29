a1d6979deafbf4fbd9858beb5958628b1b7f4f5e
Sai-Bhuvan <107638614+Sai-Bhuvan@users.noreply.github.com>
2023-01-29 11:45:18 +0530

    Update binary_search_tree.c

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -17,30 +17,95 @@ NODE insertBST(int x,NODE root){
     if(root==NULL){
         return temp;
     }
-    NODE prev,cur;
-    prev=NULL;
-    cur=root;
-    while(cur!=NULL){
-        prev=cur;
-        if(x<cur->data)
-            cur=cur->llink;
-        else
-            cur=cur->rlink;
-        if(x<prev->data){
-            prev->llink=temp;
+    else{
+        NODE p=root;
+        while(1){
+           if (x > p->data)
+            {
+                if (p->rlink == NULL)
+                {
+                    p->rlink = temp;
+                    break;
+                }
+                p = p->rlink;
+            }
+            else
+            {
+                if (p->llink == NULL)
+                {
+                    p->llink = temp;
+                    break;
+                }
+                p = p->llink;
+            }
+            
         }
-        else
-            prev->rlink=temp;
-        
+    return root;
     }
     return root;
     
 }
 
-void main(){
-    NODE y;
-    y=insertBST(7,y);
-    y=insertBST(7,y);
-    y=insertBST(8,y);
-    y=insertBST(9,y);
+void inorder(NODE root)
+{
+    if (root == NULL)
+        return;
+    else
+    {
+        inorder(root->llink);
+        printf("%d\t", root->data);
+        inorder(root->rlink);
+    }
 }
+void preorder(NODE root)
+{
+    if (root == NULL)
+        return;
+    printf("%d\t", root->data);
+    preorder(root->llink);
+    preorder(root->rlink);
+}
+void postorder(NODE root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->llink);
+    postorder(root->rlink);
+    printf("%d\t", root->data);
+}
+
+void main()
+{
+    int a, ele;
+    NODE root;
+    while (1)
+    {
+        printf("Enter 1. Insert\n2. Inorder\n3. Preorder\n4. Postorder\n");
+        printf("->\t");
+        scanf("%d", &a);
+        switch (a)
+        {
+        case 1:
+            printf("Enter the element\n");
+            scanf("%d", &ele);
+            root = insertBST(root, ele);
+            break;
+        case 2:
+            inorder(root);
+            printf("\n");
+            break;
+        case 3:
+            preorder(root);
+            printf("\n");
+            break;
+        case 4:
+            postorder(root);
+            printf("\n");
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
